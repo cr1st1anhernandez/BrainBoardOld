45dedcba120fe1523b8fc46449aecf7248d0248b
Cristian Neftali Hernandez Ramirez <cristian0402218@gmail.com>
2024-07-20 14:05:00 -0600

    🚨 Configure linter(eslint, prettier)

--- app/Providers.tsx
+++ app/Providers.tsx
@@ -1,6 +1,6 @@
-"use client";
-import { NextUIProvider } from "@nextui-org/react";
-import { useRouter } from "next/navigation";
+'use client';
+import { NextUIProvider } from '@nextui-org/react';
+import { useRouter } from 'next/navigation';
 
 export function Providers({
   children,
